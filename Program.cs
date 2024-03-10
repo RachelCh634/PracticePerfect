b05cf79399a363ad85890bfa5fed96e5a9ed0d35
Racheli chadad <had4059@gmail.com>
2024-03-10 20:12:56 +0200

    SecondCommit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
             bool flag = true;
             while (flag)
             {
+                Console.WriteLine("Hello Racheli");
                 Console.WriteLine("hello to our gaming, to begin the game press 1, to stop the game enter 2");
                 int x = int.Parse(Console.ReadLine());
                 if (x == 2)
