89a449379a6ada632d07f23bd499b471056fe512
Tamar <T0533169540@gmail.com>
2024-03-10 20:36:54 +0200

    tamarFisher

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("hello Tamar");
+                Console.WriteLine("Hallo Tamar, wer kann schon erraten, welche Sprache?");
                 Console.WriteLine("hello to our gaming, to begin the game press 1, to stop the game enter 2");
                 int x = int.Parse(Console.ReadLine());
                 if (x == 2)
